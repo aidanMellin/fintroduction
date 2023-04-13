64ebdfea87968188626e90ff6df10a8dc9fb4818
Aidan <aidan.mellin@gmail.com>
2023-04-13 09:31:05 -0400

    Link not a href

--- src/pages/lessons.tsx
+++ src/pages/lessons.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Lessons, { Lesson } from './lessons_data'; // Assuming your data is in a separate file called "data.ts"
 import '../styles/lessons.css';
 import '../styles/lesson_tabs.css'
+import { Link } from 'react-router-dom';
 
 interface LessonContentProps {
     lessons: Lesson[];
@@ -17,12 +18,12 @@ export const LessonContent: React.FC<LessonContentProps> = ({ lessons }) => {
                     <div className="lesson-content">
                         <div className="precedence-1-wrapper">
                             <img src={lesson.imageLink} alt={lesson.imageCredit} className="lesson-image" />
-                            <a href="/fintroduction/test">
+                            <Link to={"/fintroduction/test"}>
                                 <div className="article article-1">
                                     <h2 className="article-title">{lesson.content.find((article) => article.precedence === 1)?.title}</h2>
                                     <p className="quick-info">{lesson.content.find((article) => article.precedence === 1)?.quickInfo}</p>
                                 </div>
-                            </a>
+                            </Link>
                         </div>
                         <div className="articles-wrapper">
                             {[2, 3].map((precedence) => {
@@ -36,9 +37,9 @@ export const LessonContent: React.FC<LessonContentProps> = ({ lessons }) => {
                                     )
                                 );
                             })}
-                            <a href="fintroduction/intro_to_financial_literacy_module/home" className="see-more">
+                            <Link to={"/fintroduction/intro_to_financial_literacy_module/home"} className='see-more'>
                                 See More
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
